d4e0d2e5a0c3821730bced64e4f699a1dbde11c7
Jory Graham <jory.graham@gmail.com>
2021-11-22 12:48:33 -0500

    Comment out bits borking dotfiles.service

--- install-spin.sh
+++ install-spin.sh
@@ -5,12 +5,11 @@ alias sibc="bin/style --include-branch-commits"
 alias cibc="bin/check --include-branch-commits"
 alias up="bundle install; bin/rails db:migrate"
 
-
 # Install VS Code dev-test-runner extension
-git clone git@github.com:Shopify/dev-test-runner /src/github.com/shopify/dev-test-runner
-EXTENSIONS_DIR=/home/${USER}/.vscode-server/extensions
-cp "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)" "${EXTENSIONS_DIR}/dev-test-runner.vsix"
-/usr/bin/code --extensions-dir="${EXTENSIONS_DIR}" --install-extension "${EXTENSIONS_DIR}/dev-test-runner.vsix"
+# git clone git@github.com:Shopify/dev-test-runner /src/github.com/shopify/dev-test-runner
+# EXTENSIONS_DIR=/home/${USER}/.vscode-server/extensions
+# cp "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)" "${EXTENSIONS_DIR}/dev-test-runner.vsix"
+# /usr/bin/code --extensions-dir="${EXTENSIONS_DIR}" --install-extension "${EXTENSIONS_DIR}/dev-test-runner.vsix"
 
 # Setup beta flags
-~/data/default/beta_flags.sh
+# ~/data/default/beta_flags.sh
